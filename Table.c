1bd6c330ddf0555528026a3ccc8d62f0d0aa1df4
prachi-010 <81354335+prachi-010@users.noreply.github.com>
2021-05-12 14:49:38 +0530

    Update Table.c

--- Table.c
+++ Table.c
@@ -1,33 +1,17 @@
 #include<stdio.h>
-
 #include<conio.h>
 
-
-
 void main()
-
 {
-
   int i,a;
-
+  clrscr();
   
-
   printf("Enter a number to find general table");
-
-  scanf("%d",&a);
-
-  
+  scanf("%u",&a);
 
   for(i=1;i<=10;i++)
-
   {
-
-    printf("%d*%d=%u\n",a,i,a*i);
-
+  printf(" %d * %d = %u \n ",a,i,a*i);
   }
-
- 
-
   getch();
-
 }
